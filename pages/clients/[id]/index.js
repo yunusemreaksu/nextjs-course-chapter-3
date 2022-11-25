7abfb45bcc520ad2d5c11083d05257cf9ff16a46
Yunus Emre Aksu <emre_aksu@live.com>
2022-11-25 15:21:49 +0300

    Navigating programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,21 @@
+import { useRouter } from "next/router";
+
 export default function ClientProjectsPage() {
+  const router = useRouter();
+  console.log(router.query);
+  function loadProjectHandler() {
+    // router.push("/clients/emre/project-a")
+    // or:
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "emre", clientprojectid: "project-a" },
+    });
+  }
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
